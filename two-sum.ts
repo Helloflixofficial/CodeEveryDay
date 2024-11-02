4d9925a613b9facff21ddf9c218b71017ff221b3
Helloflix <shboby71@gmail.com>
2024-11-02 22:15:08 +0530

    Create two-sum.ts

---
+++ two-sum.ts
@@ -0,0 +1,16 @@
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+
+        if (map.has(complement)) {
+            return [map.get(complement)!, i]; 
+        }
+
+        map.set(nums[i], i); 
+    }
+
+    throw new Error("No two sum solution"); 
+}
