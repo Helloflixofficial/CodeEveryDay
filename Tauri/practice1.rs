b855f91e481dbb503296255a67b880ca408ad02b
Helloflix <shboby71@gmail.com>
2025-04-23 22:13:14 +0530

    Create practice1.rs

---
+++ Tauri/practice1.rs
@@ -0,0 +1,11 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    app_lib::run();
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! 👋 from Rust!", name)
+}
