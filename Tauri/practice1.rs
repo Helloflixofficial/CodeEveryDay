7b6e9c04c292381e939de3877adf76ec0be4239f
Helloflix <shboby71@gmail.com>
2025-04-24 22:25:05 +0530

    Update practice1.rs

--- Tauri/practice1.rs
+++ Tauri/practice1.rs
@@ -9,3 +9,26 @@ fn main() {
 fn greet(name: &str) -> String {
     format!("Hello, {}! 👋 from Rust!", name)
 }
+
+
+
+
+struct Solution;
+impl Solution {
+    fn reverse_string(s: &str) -> String {
+        s.chars().rev().collect()
+    }
+}
+fn main() {
+    let data = "Hello sire";
+
+    let result1 = Solution::reverse_string(data);
+    println!("Total subarrays with sum {} ", result1);
+}
+
+
+fn is_palindrome(s: &str) -> bool {
+    let filtered: String = s.chars().filter(|c| c.is_alphanumeric()).collect::<String>().to_lowercase();
+    filtered == filtered.chars().rev().collect::<String>()
+}
+
