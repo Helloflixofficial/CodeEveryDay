c0fac4806a6d1138ce552fa7f047c5bac6343b65
Helloflix <shboby71@gmail.com>
2025-05-25 22:38:57 +0530

    Create databs.vb.ts

---
+++ vb/databs.vb.ts
@@ -0,0 +1,44 @@
+Imports System.Data.SqlClient
+
+Module InsertRecordExample
+    Sub Main()
+   
+        Dim connectionString As String = "Data Source=YOUR_SERVER_NAME;Initial Catalog=YourDatabaseName;Integrated Security=True"
+
+       
+        Console.Write("Enter Student Name: ")
+        Dim name As String = Console.ReadLine()
+
+        Console.Write("Enter Student Age: ")
+        Dim age As Integer = Integer.Parse(Console.ReadLine())
+
+        
+        Dim query As String = "INSERT INTO Students (Name, Age) VALUES (@Name, @Age)"
+
+     
+        Using conn As New SqlConnection(connectionString)
+            Try
+                conn.Open()
+                Using cmd As New SqlCommand(query, conn)
+                    ' Add parameters to avoid SQL injection
+                    cmd.Parameters.AddWithValue("@Name", name)
+                    cmd.Parameters.AddWithValue("@Age", age)
+
+                    ' Execute query
+                    Dim rowsInserted As Integer = cmd.ExecuteNonQuery()
+
+                    ' Confirm insertion
+                    If rowsInserted > 0 Then
+                        Console.WriteLine("Record inserted successfully.")
+                    Else
+                        Console.WriteLine("Insertion failed.")
+                    End If
+                End Using
+            Catch ex As Exception
+                Console.WriteLine("Error: " & ex.Message)
+            End Try
+        End Using
+
+        Console.ReadLine()
+    End Sub
+End Module
