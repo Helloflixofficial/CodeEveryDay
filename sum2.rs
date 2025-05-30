0e29d5e1c1fa8eb5bfda12da0091a601e0261f79
Ɓʘɓƴ <shboby71@gmail.com>
2025-05-30 14:30:00 +0530

    Add two sum Rust solution

--- sum2.rs
+++ sum2.rs
@@ -15,11 +15,3 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
     vec![]
 }
-
-fn main() {
-    let nums = vec![2, 7, 11, 15];
-    let target = 9;
-
-    let result = two_sum(nums, target);
-    println!("Indices: {:?}", result);
-}
